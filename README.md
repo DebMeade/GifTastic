6fdeebcfb9c0f32b2e328e2ed23b492634f399c4
unknown <dkmeade@gmail.com>
2018-08-14 19:42:03 -0600

    adding files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # GifTastic
+This is a page where users can click on the buttons associated to different celebrities and see 10 gif associated to each celebrity.  The user is also able to add new celebrities to the list if they want to see gifs for a different celebrity.  When the page loads each giphy is a still image.  If the user would like to see the animated version they can click on the image and it will change to the animated version.  If they would like to see the still image again that can be acheived by clicking on the picture again.
+
+The giphy's are pulled from giphy.com using an API key acquired from giphy.com.  The still images and animated images were selected by referencing their location in JavaScript and then referencing a click function.   The page was built using HTML, CSS, SavaScript and jQuery.
+
+The game can be accessed by vising the following URL:
+https://debmeade.github.io/GifTastic/
+
---
+++ assets/javascript/script.js
@@ -0,0 +1,93 @@
+$(document).ready(function() {
+
+    // Call the GIPHY API and use JavaScript and jQuery to change the HTML of the site
+// API Key: mapfgOiQSs9AVBJDK0zgffvEFkvhLLpQ
+
+// Step 1:
+//Create variable called topics to include an array of strings related to the topic
+
+
+    var celebrityName = ["Leonardo DiCaprio", "Emma Stone", "Steve Carell", "Ryan Gosling", "Jennifer Lawrence", "Bill Murray"];
+
+    $(document).on("click", ".celebrityName", function() {
+        // function displayCelebrityInfo() {
+    var celebrityName = $(this).attr("data-celebrity");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + celebrityName + "&api_key=mapfgOiQSs9AVBJDK0zgffvEFkvhLLpQ&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+
+        var results = response.data;
+        for (var i=0; i<results.length; i++) {
+
+        var celebrityDiv = $("<div class='celebrityDiv'>");
+        var p = $("<p>").text("Rating: " + results[i].rating);
+
+        var addImage = $("<img>");
+        addImage.addClass("gif");
+        addImage.attr("src", results[i].images.fixed_height_still.url);
+        addImage.attr("data-animate", results[i].images.fixed_height.url);
+        addImage.attr("data-still", results[i].images.fixed_height_still.url);
+
+        addImage.attr("data-state", "still");
+
+
+
+        celebrityDiv.append(addImage, p);
+        $("#newGifs").prepend(celebrityDiv);
+        }
+    })
+// }   //ends displayCelebrityInfo Function
+})  //ends button on click
+
+
+function renderButtons() {
+    $("#buttons").empty();
+
+    for (var i=0; i<celebrityName.length; i++) {
+        var a = $("<button>");
+        a.addClass("celebrityName btn btn-light");
+        a.attr("data-celebrity", celebrityName[i]);
+        a.text(celebrityName[i]);
+        $("#buttons").append(a);
+    }
+}
+
+$("#add-celebrityName").on("click", function(event) {
+    event.preventDefault();
+    var celebrity = $("#celebrityName-input").val().trim();
+    a.addClass("btn btn-light");
+    celebrityName.push(celebrity);
+    renderButtons();
+});
+
+$(document).on("click", ".gif", function() {
+
+    var state = $(this).attr("data-state");
+    var imgStill = $(this).attr("data-still");
+    var imgAnimate = $(this).attr("data-animate");
+
+    if(state === "still") {
+        $(this).attr("src", imgAnimate);
+        $(this).attr("data-state", "animate");
+        console.log(imgAnimate);
+    
+    } else {
+        $(this).attr("src", imgStill);
+        $(this).attr("data-state", "still");
+    }
+
+})
+
+// $(document).on("click", ".celebrityName", displayCelebrityInfo);
+
+renderButtons();
+
+    //tried adding function on line 13 closing on 56 but it did not like that    
+
+
+
+})  //ends document.ready function
